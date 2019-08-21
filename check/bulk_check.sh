9e2212ec34b98e3c158c7f9f62d282cf851eee7a
KaisongHuang <k86huang@uwaterloo.ca>
2019-08-21 17:13:58 -0400

    Update bulk_check.sh

--- check/bulk_check.sh
+++ check/bulk_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1951..2100}:
     do
-        sbatch --account=rpp-hwheater --time=00:01:10 --job-name=r8i2p1r1_$i --mem=1G --output=r8i2p1r2.log --open-mode=append check.sh r8i2p1r1 $i
+        sbatch --account=rpp-hwheater --time=00:01:10 --job-name=r8i2p1r1_$i --mem=1G --output=r8i2p1r1.log --open-mode=append check.sh r8i2p1r1 $i
         sbatch --account=rpp-hwheater --time=00:01:10 --job-name=r8i2p1r2_$i --mem=1G --output=r8i2p1r2.log --open-mode=append check.sh r8i2p1r2 $i
         sbatch --account=rpp-hwheater --time=00:01:10 --job-name=r8i2p1r3_$i --mem=1G --output=r8i2p1r3.log --open-mode=append check.sh r8i2p1r3 $i
         sbatch --account=rpp-hwheater --time=00:01:10 --job-name=r8i2p1r4_$i --mem=1G --output=r8i2p1r4.log --open-mode=append check.sh r8i2p1r4 $i
